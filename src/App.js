c7007b7b4d4a881d4875c474e6ac7b712800fe0f
Shah786-creator <59795517+Shah786-creator@users.noreply.github.com>
2023-11-27 05:24:06 +0500

    fixhot Navigation issues

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Qrcode from './pages/Qrcode/index';
 import About from './pages/About/Index'; // Adjust the import path as per your project structure
 import Navigation from './navigation/navigation';
 import Contact from './pages/Contact';
-import Home from '../src/pages/Home/index'
+import Home from '../src/pages/Home/index';
+
 function App() {
   return (
     <div className="App">
       <Router>
-      <Navigation />
-      <Routes>
-      <Route path="/" element={<Home />} />
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/Qrcode" element={<Qrcode />} />
           <Route path="/about" element={<About />} />
-          <Route path="/Contact" element={<Contact />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
